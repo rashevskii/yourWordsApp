828071519dc1217367149f16074fe04fa714a544
Stepan Rashevskii <s.rashevskii@tezis-doc.ru>
2024-10-09 16:47:03 +0400

    Fix bug with persistgate

--- index.js
+++ index.js
@@ -3,12 +3,15 @@ import { App } from './App';
 import {name as appName} from './app.json';
 import { Provider } from 'react-redux';
 import './src/locales/i18n';
-import { store } from './src/store';
+import { store, persistor } from './src/store';
+import { PersistGate } from 'redux-persist/integration/react';
 
 const Main = () => {
   return (
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   );
 }

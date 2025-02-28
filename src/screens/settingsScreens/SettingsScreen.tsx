7449d758e0fdef852376a81b14e7e1b2e370e437
rashevskii <rashevskii@mail.ru>
2025-02-28 19:04:44 +0400

    First point in settings

--- src/screens/settingsScreens/SettingsScreen.tsx
+++ src/screens/settingsScreens/SettingsScreen.tsx
@@ -2,9 +2,10 @@ import React, { FC } from "react";
 import { NativeStackNavigationProp } from "react-native-screens/lib/typescript/native-stack/types";
 import { MainStackParamList } from "../../navigations";
 import { RouteProp } from "@react-navigation/native";
-import { View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { useTheme } from "../../hooks";
 import { globalStyles } from "../../styles";
+import { useTranslation } from "react-i18next";
 
 type SettingsNavigationProp = NativeStackNavigationProp<MainStackParamList, "Settings">;
 type SettingsRouteProp = RouteProp<MainStackParamList, "Settings">;
@@ -15,8 +16,12 @@ interface SettingsScreenProps {
 }
 
 export const SettingsScreen: FC<SettingsScreenProps> = () => {
-  const { colors: { background } } = useTheme();
+  const { colors: { background, border } } = useTheme();
+  const { t } = useTranslation();
   const { baseContainer, containerPadding } = globalStyles;
+  const {
+    blockStyles
+  } = styles;
 
   return (
     <View style={[
@@ -24,6 +29,21 @@ export const SettingsScreen: FC<SettingsScreenProps> = () => {
           containerPadding, 
           { backgroundColor: background }
         ]}>
+      <View style={[blockStyles, { borderColor: border }]}>
+        <View>
+          <Text>{t("Language being studied")}</Text>
+          <TouchableOpacity><Text>English</Text></TouchableOpacity>
+        </View>
+      </View>
+      <View style={[blockStyles, { borderColor: border }]}></View>
+      <View style={[blockStyles, { borderColor: border }]}></View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  blockStyles: {
+    borderWidth: 2,
+    borderRadius: 10
+  }
+});
